3d2a72f0a48a827543b76dfda9e75298e0ba72b5
Neha <nehatualea@gmail.com>
2023-09-20 15:46:36 +0530

    removed: margin

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import ImageSlider from './components/ImageSlider';
-
+import { data } from '../src/utils/mock';
 export default function App() {
   return (
     <div className="app">
-      <ImageSlider />
+      <ImageSlider data={data} />
     </div>
   );
 }
